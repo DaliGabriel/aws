c405a3e7e733ea5fec3074751ecc23b8930a7128
Gabriel Dalí <94218227+DaliGabriel@users.noreply.github.com>
2024-05-29 16:56:20 -0600

    Update app.py

--- StartUpGenerator/app.py
+++ StartUpGenerator/app.py
@@ -1,12 +1,12 @@
 import boto3
 import botocore.config
 import json
-import datetime
+from datetime import datetime
 
-def blog_generate_using_bedrock(blogtopic:str)-> str:
+def startup_ideas_generator_using_bedrock(bussines_area:str)-> str:
     
     prompt=f"""
-    <s>[INST]Human: Write a 200 words blog on the topic {blogtopic}
+    <s>[INST]Human: Give me 20 startup software ideas that could be developed for a one person for a new business in the following business area: {bussines_area}
     Assistant:[/INST]
     """
 
@@ -33,47 +33,47 @@ def blog_generate_using_bedrock(blogtopic:str)-> str:
         response_content=response.get('body').read()
         response_data=json.loads(response_content)
         print(response_data)
-        blog_details=response_data['generation']
-        return blog_details
+        startup_details=response_data['generation']
+        return startup_details
     except Exception as e:
-        print(f"Error generating the blog:{e}")
+        print(f"Error generating the startup ideas:{e}")
         return e
 
-def save_blog_details_s3(s3_key,s3_bucket,generate_blog):
+def save_startup_ideas_s3(s3_key,s3_bucket,generate_startup_ideas):
     s3=boto3.client('s3')
 
     try:
-        s3.put_object(Bucket = s3_bucket, Key = s3_key, Body =generate_blog )
-        print("Code saved to s3")
+        s3.put_object(Bucket = s3_bucket, Key = s3_key, Body =generate_startup_ideas )
+        print("Startup ideas saved to s3")
 
     except Exception as e:
-        print("Error when saving the code to s3")
+        print("Error when saving the startup ideas to s3")
         
 
 def lambda_handler(event, context):
     # TODO implement
     event=json.loads(event['body'])
-    blogtopic=event['blog_topic']
+    bussines_area=event['bussines_area']
 
-    generate_blog=blog_generate_using_bedrock(blogtopic=blogtopic)
+    generate_startup_ideas=startup_ideas_generator_using_bedrock(bussines_area)
 
-    if generate_blog:
+    if generate_startup_ideas:
         current_time=datetime.now().strftime('%H%M%S')
-        s3_key=f"blog-output/{current_time}.txt"
-        s3_bucket='testbucketdali'
-        save_blog_details_s3(s3_key,s3_bucket,generate_blog)
+        s3_key=f"startup_ideas/{current_time}.txt"
+        s3_bucket='your_bucket_name'
+        save_startup_ideas_s3(s3_key,s3_bucket,generate_startup_ideas)
         
-        print(generate_blog)
+        print(generate_startup_ideas)
         return{
         'statusCode':200,
-        'body':json.dumps(generate_blog)
+        'body':json.dumps(generate_startup_ideas)
         }
 
     else:
-        print("No blog was generated")
+        print("No startup was generated")
         return{
         'statusCode':400,
-        'body':json.dumps('Blog Generation is not completed')
+        'body':json.dumps('Startup Generation is not completed')
         }
 
 
